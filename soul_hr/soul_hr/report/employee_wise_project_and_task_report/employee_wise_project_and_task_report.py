c8fd0c77c601d3c68300d9ca5466d4cdfd6e318b
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2023-06-15 18:07:59 +0530

    report generation wrt filters

--- soul_hr/soul_hr/report/employee_wise_project_and_task_report/employee_wise_project_and_task_report.py
+++ soul_hr/soul_hr/report/employee_wise_project_and_task_report/employee_wise_project_and_task_report.py
@@ -12,48 +12,36 @@ def execute(filters=None):
     return  get_columns_info,data
 
 def get_data(filters=None):
-    # print("\n\n\n\n")
     employee = filters.get('employee')
-    # print("employee : ",employee)
     project = filters.get('project')
-    # print("project : ",project)
     task = filters.get('task')
-    # print("task : ",task)
     start_date = filters.get('start_date')
     end_date = filters.get('end_date')
 
     filt=[]
-    if employee == [] and project == [] and task == [] and start_date == [] and end_date == []:
-        data = frappe.get_all("Project and Tasks Report", filters=[["workflow_state","in", ["Sent for Approval","Approved"]]],
-                                                                             fields = ["name", "employee", "employee_name", "report_for_week_starting"])
-    else:
+    if employee:
+        filt.append(["employee","in",tuple(employee)])
+    if start_date and end_date:
+       filt.append(["report_for_week_starting", "between", [start_date,end_date]])     
+    filt.append(["workflow_state","in", ["Sent for Approval","Approved"]])
 
-        data = frappe.get_all("Project and Tasks Report", filters=[["employee","in",tuple(employee)], 
-                                                               ["report_for_week_starting", "between", [start_date,end_date]],["workflow_state","in", ["Sent for Approval","Approved"]]],
-                                                                              fields = ["name", "employee", "employee_name", "report_for_week_starting"])
-    # print("parent_data: ",data)
+
+    data = frappe.get_all("Project and Tasks Report", filters=filt,fields = ["name", "employee", "employee_name", "report_for_week_starting"])
+
 
     final_data=[]
     for t in data:
         flag_dict={}
-        # child_data = frappe.get_all("Project and Tasks Estimation Table", {"parent":t["name"],"project":project,"tasks":task},
-        #                             ["parent","project","project_name","tasks","task_name","task_description","total"])
-
-        # if employee == []:
-        #     print("ok")
-        #     child_data = frappe.get_all("Project and Tasks Estimation Table", filters=[["project","in",tuple(project)],["tasks","in",tuple(task)]],
-        #                     fields = ["parent","project","project_name","tasks","task_name","task_description","total"])
-        # else:
-        child_data = frappe.get_all("Project and Tasks Estimation Table", filters=[["parent","=",t["name"]],["project","in",tuple(project)],["tasks","in",tuple(task)]],
+        filter=[]
+        filter.append(["parent","=",t["name"]])
+        if project:
+            filter.append(["project","in",tuple(project)])
+        if task:
+            filter.append(["tasks","in",tuple(task)])      
+        child_data = frappe.get_all("Project and Tasks Estimation Table", filters=filter,
                             fields = ["parent","project","project_name","tasks","task_name","task_description","total"])
-        # child_data = frappe.db.sql("""SELECT `parent`,`project`,`project_name`,`tasks`,`task_name`,`task_description` FROM `tabProject and Tasks Estimation Table` 
-        #                             WHERE (`parent` = %s) AND (`project` in %s) AND (`tasks` in %s)"""%(t["name"],str(tuple(project)),str(tuple(task))))
-        
-
-        # print("child_data: ", child_data, "\n")
 
         for j in child_data:
-            # flag_dict['name']=t['name']
             flag_dict['employee']=t['employee']
             flag_dict['employee_name']=t['employee_name']
             flag_dict['report_for_week_starting']=t['report_for_week_starting']
@@ -62,7 +50,6 @@ def get_data(filters=None):
             flag_dict['project_name']=j["project_name"]
             flag_dict['tasks']=j["tasks"]
             flag_dict['task_name']=j["task_name"]
-            # flag_dict['task_description']=j["task_description"]
             flag_dict['total']=j["total"]
             temp = []
             res = dict()
@@ -76,18 +63,10 @@ def get_data(filters=None):
 
             
     sorted_final_data = (sorted(final_data, key=itemgetter('project', 'tasks')))
-    # print("final_data: ",sorted_final_data)
     return sorted_final_data
 
 def get_columns():
     columns = [
-        # {
-        #     "label": _("Project and Task ID"),
-        #     "fieldname": "name",
-        #     "fieldtype": "Link",
-        #     "options": "Project and Tasks Report",
-        #     "width":145
-        # },
         {
             "label": _("Employee"),
             "fieldname": "employee",
@@ -101,13 +80,6 @@ def get_columns():
             "fieldtype": "Date",
             "width":145
         },
-        # {
-        # 	"label": _("Employee Name"),
-        # 	"fieldname": "employee_name",
-        # 	"fieldtype": "Link",
-        # 	"options": "Employee",
-        # 	"width":180
-        # },
         {
             "label": _("Project"),
             "fieldname": "project",
@@ -132,17 +104,16 @@ def get_columns():
             "fieldtype": "Data",
             "width":150
         },
-        # {
-        #     "label": _("Task Description"),
-        #     "fieldname": "task_description",
-        #     "fieldtype": "Data",
-        #     "width":500
-        # },
         {
             "label": _("Total Hours"),
             "fieldname": "total",
             "fieldtype": "Data",
             "width":100
-        },
+        },        # {
+        #     "label": _("Task Description"),
+        #     "fieldname": "task_description",
+        #     "fieldtype": "Data",
+        #     "width":500
+        # },
     ]
     return columns
